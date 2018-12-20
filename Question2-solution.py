b3f5c553cfb1574234b4162ac245d07be85a19e8
aboud20-meet <aboud20@meet.mit.edu>
2018-12-20 19:04:41 +0200

    w

---
+++ Question2-solution.py
@@ -0,0 +1,37 @@
+import math 
+i=0
+s=0
+for i in range(1000):
+	i+=1
+	s+=i**i
+# s=(1000-1000**1001)/(-999)
+p=str(s)
+
+
+q=""
+x=10
+while x>0:
+ q=q+(p[-x])
+ x+=-1
+
+
+
+
+print(q)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
